591a5c48742b4904e420702313ee815eae154927
Viswam PC <75151315+viswc@users.noreply.github.com>
2023-09-22 11:28:17 +0530

    __Final

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ cd /blog
 sudo pip3 install -r requirements.txt
 cd /blog/Intern-blog
 python3 manage.py collectstatic --noinput
-python3 manage.py runserver
+nohup python3 manage.py runserver &
 
 rm -f /etc/nginx/sites-enabled/default
 cp /blog/nginx.conf /etc/nginx/sites-enabled/
